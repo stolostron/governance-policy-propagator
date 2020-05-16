11caf9c9b470c29940440708d29179197fe323c1
Yu Cao <ycao@redhat.com>
2020-05-16 10:21:06 -0400

    fix

--- build/run-unit-tests.sh
+++ build/run-unit-tests.sh
@@ -34,7 +34,6 @@ make build-instrumented
 make kind-bootstrap-cluster-dev
 make run-instrumented
 make e2e-test
-go test -json `go list ./... | grep -v test/e2e` > report.json
 make stop-instrumented || true
 # sleep 10
 # echo "Checking coverage.out ..."

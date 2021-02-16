8c6b8a768f1e985fbfa2254db14877425bb6dc5e
Yu Cao <ycao@redhat.com>
2021-02-16 09:59:06 -0500

    revert

--- pkg/controller/propagator/policy_propagator_controller.go
+++ pkg/controller/propagator/policy_propagator_controller.go
@@ -15,6 +15,7 @@ import (
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -53,7 +54,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Watch for changes to primary resource Policy
 	err = c.Watch(
 		&source.Kind{Type: &policiesv1.Policy{}},
-		&common.EnqueueRequestsFromMapFunc{ToRequests: &policyMapper{mgr.GetClient()}})
+		&handler.EnqueueRequestsFromMapFunc{ToRequests: &policyMapper{mgr.GetClient()}})
 	if err != nil {
 		return err
 	}
@@ -61,7 +62,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Watch for changes to placementbinding
 	err = c.Watch(
 		&source.Kind{Type: &policiesv1.PlacementBinding{}},
-		&common.EnqueueRequestsFromMapFunc{ToRequests: &placementBindingMapper{mgr.GetClient()}}, pbPredicateFuncs)
+		&handler.EnqueueRequestsFromMapFunc{ToRequests: &placementBindingMapper{mgr.GetClient()}}, pbPredicateFuncs)
 	if err != nil {
 		return err
 	}

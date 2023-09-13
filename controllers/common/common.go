JustinKuli/governance-policy-propagator-1

Refactor: typos and re-used code
The new `IsForPolicyOrPolicySet` may be very slightly faster than the
previous implementation. The other changes should have basically no
effect except be a little bit easier to read.

Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

controllers/common/common.go
@@ -17,8 +17,10 @@ import (
 	clusterv1beta1 "open-cluster-management.io/api/cluster/v1beta1"
 	appsv1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/placementrule/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
+	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
 )
 
 const (
@@ -60,24 +62,25 @@ func IsReplicatedPolicy(c client.Client, policy client.Object) (bool, error) {
 	return IsInClusterNamespace(c, policy.GetNamespace())
 }
 
-// IsPbForPoicy compares group and kind with policy group and kind for given pb
-func IsPbForPoicy(pb *policiesv1.PlacementBinding) bool {
-	found := false
+// IsForPolicyOrPolicySet returns true if any of the subjects of the PlacementBinding are Policies
+// or PolicySets.
+func IsForPolicyOrPolicySet(pb *policiesv1.PlacementBinding) bool {
+	if pb == nil {
+		return false
+	}
 
-	subjects := pb.Subjects
-	for _, subject := range subjects {
-		if subject.Kind == policiesv1.Kind && subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-			found = true
-
-			break
+	for _, subject := range pb.Subjects {
+		if subject.APIGroup == policiesv1.SchemeGroupVersion.Group &&
+			(subject.Kind == policiesv1.Kind || subject.Kind == policiesv1.PolicySetKind) {
+			return true
 		}
 	}
 
-	return found
+	return false
 }
 
-// IsPbForPoicySet compares group and kind with policyset group and kind for given pb
-func IsPbForPoicySet(pb *policiesv1.PlacementBinding) bool {
+// IsPbForPolicySet compares group and kind with policyset group and kind for given pb
+func IsPbForPolicySet(pb *policiesv1.PlacementBinding) bool {
 	found := false
 
 	subjects := pb.Subjects
@@ -90,6 +93,48 @@ func IsPbForPoicySet(pb *policiesv1.PlacementBinding) bool {
 	}
 
 	return found
+}
+
+// PoliciesInPlacementBinding returns a list of the Policies that are either direct subjects of
+// the given PlacementBinding, or are in PolicySets that are subjects of the PlacementBinding.
+// The list items are not guaranteed to be unique (for example if a policy is in multiple sets).
+func PoliciesInPlacementBinding(
+	ctx context.Context, c client.Client, pb *policiesv1.PlacementBinding,
+) []reconcile.Request {
+	result := make([]reconcile.Request, 0)
+
+	for _, subject := range pb.Subjects {
+		if subject.APIGroup != policiesv1.SchemeGroupVersion.Group {
+			continue
+		}
+
+		switch subject.Kind {
+		case policiesv1.Kind:
+			result = append(result, reconcile.Request{NamespacedName: types.NamespacedName{
+				Name:      subject.Name,
+				Namespace: pb.GetNamespace(),
+			}})
+		case policiesv1.PolicySetKind:
+			setNN := types.NamespacedName{
+				Name:      subject.Name,
+				Namespace: pb.GetNamespace(),
+			}
+
+			policySet := policiesv1beta1.PolicySet{}
+			if err := c.Get(ctx, setNN, &policySet); err != nil {
+				continue
+			}
+
+			for _, plc := range policySet.Spec.Policies {
+				result = append(result, reconcile.Request{NamespacedName: types.NamespacedName{
+					Name:      string(plc),
+					Namespace: pb.GetNamespace(),
+				}})
+			}
+		}
+	}
+
+	return result
 }
 
 // FindNonCompliantClustersForPolicy returns cluster in noncompliant status with given policy

controllers/propagator/placementBindingMapper.go
@@ -6,64 +6,22 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementBindingMapper(c client.Client) handler.MapFunc {
 	return func(ctx context.Context, obj client.Object) []reconcile.Request {
 		//nolint:forcetypeassert
-		object := obj.(*policiesv1.PlacementBinding)
-		var result []reconcile.Request
+		pb := obj.(*policiesv1.PlacementBinding)
 
-		log := log.WithValues("placementBindingName", object.GetName(), "namespace", object.GetNamespace())
-
+		log := log.WithValues("placementBindingName", pb.GetName(), "namespace", pb.GetNamespace())
 		log.V(2).Info("Reconcile request for a PlacementBinding")
 
-		subjects := object.Subjects
-		for _, subject := range subjects {
-			if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-				if subject.Kind == policiesv1.Kind {
-					log.V(2).Info("Found reconciliation request from policy placement binding",
-						"policyName", subject.Name)
-
-					request := reconcile.Request{NamespacedName: types.NamespacedName{
-						Name:      subject.Name,
-						Namespace: object.GetNamespace(),
-					}}
-					result = append(result, request)
-				} else if subject.Kind == policiesv1.PolicySetKind {
-					policySetNamespacedName := types.NamespacedName{
-						Name:      subject.Name,
-						Namespace: object.GetNamespace(),
-					}
-					policySet := &policiesv1beta1.PolicySet{}
-					err := c.Get(ctx, policySetNamespacedName, policySet)
-					if err != nil {
-						log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-							"policySetName", subject.Name, "error", err)
-
-						continue
-					}
-					policies := policySet.Spec.Policies
-					for _, plc := range policies {
-						log.V(2).Info("Found reconciliation request from policyset placement bindng",
-							"policySetName", subject.Name, "policyName", string(plc))
-						request := reconcile.Request{NamespacedName: types.NamespacedName{
-							Name:      string(plc),
-							Namespace: object.GetNamespace(),
-						}}
-						result = append(result, request)
-					}
-				}
-			}
-		}
-
-		return result
+		return common.PoliciesInPlacementBinding(ctx, c, pb)
 	}
 }

controllers/propagator/placementDecisionMapper.go
@@ -6,14 +6,13 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	clusterv1beta1 "open-cluster-management.io/api/cluster/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementDecisionMapper(c client.Client) handler.MapFunc {
@@ -40,52 +39,14 @@ func placementDecisionMapper(c client.Client) handler.MapFunc {
 		}
 
 		var result []reconcile.Request
-		// loop through pb to find if current placement is used for policy
+		// loop through pbs and collect policies from each matching one.
 		for _, pb := range pbList.Items {
 			if pb.PlacementRef.APIGroup != clusterv1beta1.SchemeGroupVersion.Group ||
 				pb.PlacementRef.Kind != "Placement" || pb.PlacementRef.Name != placementName {
 				continue
 			}
 
-			// found matching placement in pb -- check if it is for policy or policyset
-			subjects := pb.Subjects
-			for _, subject := range subjects {
-				if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-					if subject.Kind == policiesv1.Kind {
-						log.V(2).Info("Found reconciliation request from policy a placement decision",
-							"policyName", subject.Name)
-						// generate reconcile request for policy referenced by pb
-						request := reconcile.Request{NamespacedName: types.NamespacedName{
-							Name:      subject.Name,
-							Namespace: object.GetNamespace(),
-						}}
-						result = append(result, request)
-					} else if subject.Kind == policiesv1.PolicySetKind {
-						policySetNamespacedName := types.NamespacedName{
-							Name:      subject.Name,
-							Namespace: object.GetNamespace(),
-						}
-						policySet := &policiesv1beta1.PolicySet{}
-						err := c.Get(ctx, policySetNamespacedName, policySet)
-						if err != nil {
-							log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-								"policySetName", subject.Name, "placementBindingName", pb.GetName(), "error", err)
-
-							continue
-						}
-						policies := policySet.Spec.Policies
-						for _, plc := range policies {
-							log.V(2).Info("Found reconciliation request from a policyset placement decision",
-								"policySetName", subject.Name, "policyName", string(plc))
-							request := reconcile.Request{NamespacedName: types.NamespacedName{
-								Name:      string(plc),
-								Namespace: object.GetNamespace(),
-							}}
-							result = append(result, request)
-						}
-					}
-				}
-			}
+			result = append(result, common.PoliciesInPlacementBinding(ctx, c, &pb)...)
 		}
 
 		return result

controllers/propagator/placementRuleMapper.go
@@ -6,14 +6,13 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	appsv1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/placementrule/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementRuleMapper(c client.Client) handler.MapFunc {
@@ -32,51 +31,14 @@ func placementRuleMapper(c client.Client) handler.MapFunc {
 		}
 
 		var result []reconcile.Request
-		// loop through pb to find if current placementrule is used for policy
+		// loop through pbs and collect policies from each matching one.
 		for _, pb := range pbList.Items {
-			// found matching placement rule in pb
-			if pb.PlacementRef.APIGroup == appsv1.SchemeGroupVersion.Group &&
-				pb.PlacementRef.Kind == "PlacementRule" && pb.PlacementRef.Name == object.GetName() {
-				// check if it is for policy
-				subjects := pb.Subjects
-				for _, subject := range subjects {
-					if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-						if subject.Kind == policiesv1.Kind {
-							log.V(2).Info("Found reconciliation request from policy placement rule", "policyName",
-								subject.Name)
-							// generate reconcile request for policy referenced by pb
-							request := reconcile.Request{NamespacedName: types.NamespacedName{
-								Name:      subject.Name,
-								Namespace: object.GetNamespace(),
-							}}
-							result = append(result, request)
-						} else if subject.Kind == policiesv1.PolicySetKind {
-							policySetNamespacedName := types.NamespacedName{
-								Name:      subject.Name,
-								Namespace: object.GetNamespace(),
-							}
-							policySet := &policiesv1beta1.PolicySet{}
-							err := c.Get(ctx, policySetNamespacedName, policySet)
-							if err != nil {
-								log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-									"policySetName", subject.Name, "placementBindingName", pb.GetName(), "error", err)
-
-								continue
-							}
-							policies := policySet.Spec.Policies
-							for _, plc := range policies {
-								log.V(2).Info("Found reconciliation request from a policyset placement rule",
-									"policySetName", subject.Name, "policyName", string(plc))
-								request := reconcile.Request{NamespacedName: types.NamespacedName{
-									Name:      string(plc),
-									Namespace: object.GetNamespace(),
-								}}
-								result = append(result, request)
-							}
-						}
-					}
-				}
+			if pb.PlacementRef.APIGroup != appsv1.SchemeGroupVersion.Group ||
+				pb.PlacementRef.Kind != "PlacementRule" || pb.PlacementRef.Name != object.GetName() {
+				continue
 			}
+
+			result = append(result, common.PoliciesInPlacementBinding(ctx, c, &pb)...)
 		}
 
 		return result

stolostron/governance-policy-propagator

previous implementation.
The `GetPoliciesInPlacementBinding` function is from duplicated code.
The removal of a `DeepCopy()` call may improve performance.
(cherry picked from commit fdc3118f193f729d0b6d9ca50ecc1d4e1f35002c)

controllers/common/common.go
@@ -17,8 +17,10 @@ import (
 	clusterv1beta1 "open-cluster-management.io/api/cluster/v1beta1"
 	appsv1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/placementrule/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
+	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
 )
 
 const (
@@ -60,36 +62,79 @@ func IsReplicatedPolicy(c client.Client, policy client.Object) (bool, error) {
 	return IsInClusterNamespace(c, policy.GetNamespace())
 }
 
-// IsPbForPoicy compares group and kind with policy group and kind for given pb
-func IsPbForPoicy(pb *policiesv1.PlacementBinding) bool {
-	found := false
+// IsForPolicyOrPolicySet returns true if any of the subjects of the PlacementBinding are Policies
+// or PolicySets.
+func IsForPolicyOrPolicySet(pb *policiesv1.PlacementBinding) bool {
+	if pb == nil {
+		return false
+	}
 
-	subjects := pb.Subjects
-	for _, subject := range subjects {
-		if subject.Kind == policiesv1.Kind && subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-			found = true
-
-			break
+	for _, subject := range pb.Subjects {
+		if subject.APIGroup == policiesv1.SchemeGroupVersion.Group &&
+			(subject.Kind == policiesv1.Kind || subject.Kind == policiesv1.PolicySetKind) {
+			return true
 		}
 	}
 
-	return found
+	return false
 }
 
-// IsPbForPoicySet compares group and kind with policyset group and kind for given pb
-func IsPbForPoicySet(pb *policiesv1.PlacementBinding) bool {
-	found := false
+// IsPbForPolicySet compares group and kind with policyset group and kind for given pb
+func IsPbForPolicySet(pb *policiesv1.PlacementBinding) bool {
+	if pb == nil {
+		return false
+	}
 
 	subjects := pb.Subjects
 	for _, subject := range subjects {
 		if subject.Kind == policiesv1.PolicySetKind && subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-			found = true
-
-			break
+			return true
 		}
 	}
 
-	return found
+	return false
+}
+
+// GetPoliciesInPlacementBinding returns a list of the Policies that are either direct subjects of
+// the given PlacementBinding, or are in PolicySets that are subjects of the PlacementBinding.
+// The list items are not guaranteed to be unique (for example if a policy is in multiple sets).
+func GetPoliciesInPlacementBinding(
+	ctx context.Context, c client.Client, pb *policiesv1.PlacementBinding,
+) []reconcile.Request {
+	result := make([]reconcile.Request, 0)
+
+	for _, subject := range pb.Subjects {
+		if subject.APIGroup != policiesv1.SchemeGroupVersion.Group {
+			continue
+		}
+
+		switch subject.Kind {
+		case policiesv1.Kind:
+			result = append(result, reconcile.Request{NamespacedName: types.NamespacedName{
+				Name:      subject.Name,
+				Namespace: pb.GetNamespace(),
+			}})
+		case policiesv1.PolicySetKind:
+			setNN := types.NamespacedName{
+				Name:      subject.Name,
+				Namespace: pb.GetNamespace(),
+			}
+
+			policySet := policiesv1beta1.PolicySet{}
+			if err := c.Get(ctx, setNN, &policySet); err != nil {
+				continue
+			}
+
+			for _, plc := range policySet.Spec.Policies {
+				result = append(result, reconcile.Request{NamespacedName: types.NamespacedName{
+					Name:      string(plc),
+					Namespace: pb.GetNamespace(),
+				}})
+			}
+		}
+	}
+
+	return result
 }
 
 // FindNonCompliantClustersForPolicy returns cluster in noncompliant status with given policy

controllers/propagator/placementBindingMapper.go
@@ -6,64 +6,22 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementBindingMapper(c client.Client) handler.MapFunc {
 	return func(ctx context.Context, obj client.Object) []reconcile.Request {
 		//nolint:forcetypeassert
-		object := obj.(*policiesv1.PlacementBinding)
-		var result []reconcile.Request
+		pb := obj.(*policiesv1.PlacementBinding)
 
-		log := log.WithValues("placementBindingName", object.GetName(), "namespace", object.GetNamespace())
-
+		log := log.WithValues("placementBindingName", pb.GetName(), "namespace", pb.GetNamespace())
 		log.V(2).Info("Reconcile request for a PlacementBinding")
 
-		subjects := object.Subjects
-		for _, subject := range subjects {
-			if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-				if subject.Kind == policiesv1.Kind {
-					log.V(2).Info("Found reconciliation request from policy placement binding",
-						"policyName", subject.Name)
-
-					request := reconcile.Request{NamespacedName: types.NamespacedName{
-						Name:      subject.Name,
-						Namespace: object.GetNamespace(),
-					}}
-					result = append(result, request)
-				} else if subject.Kind == policiesv1.PolicySetKind {
-					policySetNamespacedName := types.NamespacedName{
-						Name:      subject.Name,
-						Namespace: object.GetNamespace(),
-					}
-					policySet := &policiesv1beta1.PolicySet{}
-					err := c.Get(ctx, policySetNamespacedName, policySet)
-					if err != nil {
-						log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-							"policySetName", subject.Name, "error", err)
-
-						continue
-					}
-					policies := policySet.Spec.Policies
-					for _, plc := range policies {
-						log.V(2).Info("Found reconciliation request from policyset placement bindng",
-							"policySetName", subject.Name, "policyName", string(plc))
-						request := reconcile.Request{NamespacedName: types.NamespacedName{
-							Name:      string(plc),
-							Namespace: object.GetNamespace(),
-						}}
-						result = append(result, request)
-					}
-				}
-			}
-		}
-
-		return result
+		return common.GetPoliciesInPlacementBinding(ctx, c, pb)
 	}
 }

controllers/propagator/placementDecisionMapper.go
@@ -6,14 +6,13 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	clusterv1beta1 "open-cluster-management.io/api/cluster/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementDecisionMapper(c client.Client) handler.MapFunc {
@@ -40,52 +39,14 @@ func placementDecisionMapper(c client.Client) handler.MapFunc {
 		}
 
 		var result []reconcile.Request
-		// loop through pb to find if current placement is used for policy
+		// loop through pbs and collect policies from each matching one.
 		for _, pb := range pbList.Items {
 			if pb.PlacementRef.APIGroup != clusterv1beta1.SchemeGroupVersion.Group ||
 				pb.PlacementRef.Kind != "Placement" || pb.PlacementRef.Name != placementName {
 				continue
 			}
 
-			// found matching placement in pb -- check if it is for policy or policyset
-			subjects := pb.Subjects
-			for _, subject := range subjects {
-				if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-					if subject.Kind == policiesv1.Kind {
-						log.V(2).Info("Found reconciliation request from policy a placement decision",
-							"policyName", subject.Name)
-						// generate reconcile request for policy referenced by pb
-						request := reconcile.Request{NamespacedName: types.NamespacedName{
-							Name:      subject.Name,
-							Namespace: object.GetNamespace(),
-						}}
-						result = append(result, request)
-					} else if subject.Kind == policiesv1.PolicySetKind {
-						policySetNamespacedName := types.NamespacedName{
-							Name:      subject.Name,
-							Namespace: object.GetNamespace(),
-						}
-						policySet := &policiesv1beta1.PolicySet{}
-						err := c.Get(ctx, policySetNamespacedName, policySet)
-						if err != nil {
-							log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-								"policySetName", subject.Name, "placementBindingName", pb.GetName(), "error", err)
-
-							continue
-						}
-						policies := policySet.Spec.Policies
-						for _, plc := range policies {
-							log.V(2).Info("Found reconciliation request from a policyset placement decision",
-								"policySetName", subject.Name, "policyName", string(plc))
-							request := reconcile.Request{NamespacedName: types.NamespacedName{
-								Name:      string(plc),
-								Namespace: object.GetNamespace(),
-							}}
-							result = append(result, request)
-						}
-					}
-				}
-			}
+			result = append(result, common.GetPoliciesInPlacementBinding(ctx, c, &pb)...)
 		}
 
 		return result

controllers/propagator/placementRuleMapper.go
@@ -6,14 +6,13 @@ package propagator
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/types"
 	appsv1 "open-cluster-management.io/multicloud-operators-subscription/pkg/apis/apps/placementrule/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	policiesv1 "open-cluster-management.io/governance-policy-propagator/api/v1"
-	policiesv1beta1 "open-cluster-management.io/governance-policy-propagator/api/v1beta1"
+	"open-cluster-management.io/governance-policy-propagator/controllers/common"
 )
 
 func placementRuleMapper(c client.Client) handler.MapFunc {
@@ -32,51 +31,14 @@ func placementRuleMapper(c client.Client) handler.MapFunc {
 		}
 
 		var result []reconcile.Request
-		// loop through pb to find if current placementrule is used for policy
+		// loop through pbs and collect policies from each matching one.
 		for _, pb := range pbList.Items {
-			// found matching placement rule in pb
-			if pb.PlacementRef.APIGroup == appsv1.SchemeGroupVersion.Group &&
-				pb.PlacementRef.Kind == "PlacementRule" && pb.PlacementRef.Name == object.GetName() {
-				// check if it is for policy
-				subjects := pb.Subjects
-				for _, subject := range subjects {
-					if subject.APIGroup == policiesv1.SchemeGroupVersion.Group {
-						if subject.Kind == policiesv1.Kind {
-							log.V(2).Info("Found reconciliation request from policy placement rule", "policyName",
-								subject.Name)
-							// generate reconcile request for policy referenced by pb
-							request := reconcile.Request{NamespacedName: types.NamespacedName{
-								Name:      subject.Name,
-								Namespace: object.GetNamespace(),
-							}}
-							result = append(result, request)
-						} else if subject.Kind == policiesv1.PolicySetKind {
-							policySetNamespacedName := types.NamespacedName{
-								Name:      subject.Name,
-								Namespace: object.GetNamespace(),
-							}
-							policySet := &policiesv1beta1.PolicySet{}
-							err := c.Get(ctx, policySetNamespacedName, policySet)
-							if err != nil {
-								log.V(2).Info("Failed to retrieve policyset referenced in placementbinding",
-									"policySetName", subject.Name, "placementBindingName", pb.GetName(), "error", err)
-
-								continue
-							}
-							policies := policySet.Spec.Policies
-							for _, plc := range policies {
-								log.V(2).Info("Found reconciliation request from a policyset placement rule",
-									"policySetName", subject.Name, "policyName", string(plc))
-								request := reconcile.Request{NamespacedName: types.NamespacedName{
-									Name:      string(plc),
-									Namespace: object.GetNamespace(),
-								}}
-								result = append(result, request)
-							}
-						}
-					}
-				}
+			if pb.PlacementRef.APIGroup != appsv1.SchemeGroupVersion.Group ||
+				pb.PlacementRef.Kind != "PlacementRule" || pb.PlacementRef.Name != object.GetName() {
+				continue
 			}
+
+			result = append(result, common.GetPoliciesInPlacementBinding(ctx, c, &pb)...)
 		}
 
 		return result